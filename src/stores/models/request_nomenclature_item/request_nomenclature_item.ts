6fc789bd87ed906d84a21bef2fa50e4b8082ccb0
ujin <ujinael@gmail.com>
2022-08-10 00:17:59 +0300

    new

--- src/stores/models/request_nomenclature_item/request_nomenclature_item.ts
+++ src/stores/models/request_nomenclature_item/request_nomenclature_item.ts
@@ -2,11 +2,5 @@
 import { Type } from "class-transformer"
 export class RequestNomenclatureItem {
 id?:string
-@Type(()=>NomenclatureItem)
-nomenclatureItem:NomenclatureItem
-count:number
-rub:number
-penny:number
-
 }
 
